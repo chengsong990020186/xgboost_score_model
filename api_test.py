4b6a43c15853a6696fbdf9afdbf78a629917767b
chengsong <990020186@qq.com>
2019-05-31 16:41:07 +0800

    Add files via upload

---
+++ api_test.py
@@ -0,0 +1,21 @@
+import requests
+import json
+data_sample='''
+{
+    "user_id": "201321420",
+    "user_data": {"DebtRatio": 0.80,
+				 "MonthlyIncome": 9120.0,
+				 "NumberOfDependents": 2.0,
+				 "NumberOfOpenCreditLinesAndLoans": 13.0,
+				 "NumberOfTime30-59DaysPastDueNotWorse": 2.0,
+				 "NumberOfTime60-89DaysPastDueNotWorse": 0.0,
+				 "NumberOfTimes90DaysLate": 0.0,
+				 "NumberRealEstateLoansOrLines": 6.0,
+				 "RevolvingUtilizationOfUnsecuredLines": 0.766,
+				 "age": 45.0}
+}
+'''
+data=json.loads(data_sample)
+url='http://127.0.0.1:8000/xgboost_score_model/v1.0'
+page=requests.post(url,json=data) 
+print(page.json())
